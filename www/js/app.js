83e1a671b0d760b403992b60ee59edad3eabf633
jeshan <j@code.jeshan.co>
2016-05-02 21:11:31 +0400

    basic home page: added cards for each topic

--- www/js/app.js
+++ www/js/app.js
@@ -6,80 +6,80 @@
 var app = angular.module('starter', ['ionic', 'ionic-material']);
 
 app.run(function ($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
+  $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
 
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
-})
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $stateProvider
+  $stateProvider
 
     .state('app', {
-        url: '/app',
-        abstract: true,
-        templateUrl: 'templates/menu.html',
-        controller: 'AppCtrl'
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
     })
 
     .state('app.lists', {
-        url: '/lists',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/lists.html',
-                controller: 'ListsCtrl'
-            }
+      url: '/lists',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/lists.html',
+          controller: 'ListsCtrl'
         }
+      }
     })
 
     .state('app.ink', {
-        url: '/ink',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/ink.html',
-                controller: 'InkCtrl'
-            }
+      url: '/ink',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/ink.html',
+          controller: 'InkCtrl'
         }
+      }
     })
 
     .state('app.motion', {
-        url: '/motion',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/motion.html',
-                controller: 'MotionCtrl'
-            }
+      url: '/motion',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/motion.html',
+          controller: 'MotionCtrl'
         }
+      }
     })
 
-    .state('app.components', {
-        url: '/components',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/components.html',
-                controller: 'ComponentsCtrl'
-            }
+    .state('app.home', {
+      url: '/home',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/home.html',
+          controller: 'HomeCtrl'
         }
+      }
     })
 
     .state('app.extensions', {
-        url: '/extensions',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/extensions.html',
-                controller: 'ExtensionsCtrl'
-            }
+      url: '/extensions',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/extensions.html',
+          controller: 'ExtensionsCtrl'
         }
+      }
     })
-    ;
+  ;
 
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/components');
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
 });
