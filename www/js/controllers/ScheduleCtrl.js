3dd2a306e772042e70fd3d5a83fe47ea7c6169b2
jeshan <j@code.jeshan.co>
2016-05-04 00:30:25 +0400

    schedule page: integration with Google Docs api

--- www/js/controllers/ScheduleCtrl.js
+++ www/js/controllers/ScheduleCtrl.js
@@ -1,6 +1,16 @@
-﻿app.controller('ScheduleCtrl', function ($scope, $stateParams, ionicMaterialInk) {
+﻿app.controller('ScheduleCtrl', function ($scope, $stateParams, ionicMaterialInk, sessionService, $timeout) {
   //ionic.material.ink.displayEffect();
   ionicMaterialInk.displayEffect();
 
-
+  $scope.events = [];
+  $scope.selectTab = function (index) {
+    $timeout(function () {
+      console.log("index", index);
+      if (!$scope.events[index - 1]) {
+        sessionService.getList(index).then(function (response) {
+          $scope.events[index - 1] = response;
+        });
+      }
+    });
+  };
 });
