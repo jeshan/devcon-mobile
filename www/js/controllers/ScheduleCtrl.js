ac769cc488abcb99b46d21693d32acb8eda74a88
jeshan <j@code.jeshan.co>
2016-05-09 23:22:41 +0400

    show event details

--- www/js/controllers/ScheduleCtrl.js
+++ www/js/controllers/ScheduleCtrl.js
@@ -1,4 +1,4 @@
-﻿app.controller('ScheduleCtrl', function ($scope, $stateParams, ionicMaterialInk, sessionService, $timeout) {
+﻿app.controller('ScheduleCtrl', function ($scope, $stateParams, ionicMaterialInk, sessionService, $timeout, $ionicPopup) {
   //ionic.material.ink.displayEffect();
   ionicMaterialInk.displayEffect();
 
@@ -11,6 +11,20 @@
           $scope.events[index - 1] = response;
         });
       }
+    });
+  };
+
+  $scope.showEventDetails = function (event) {
+    if (!event.abstract) {
+      $ionicPopup.alert({
+        title: 'Error',
+        template: 'No info available for this session :('
+      });
+      return;
+    }
+    $ionicPopup.alert({
+      title: event.title,
+      template: event.abstract
     });
   };
 });
