cdbbfb9c425afa7f48295db58805d329c51a2e12
jeshan <j@code.jeshan.co>
2016-05-14 11:03:06 +0400

    add caching of session data

--- www/js/controllers/ScheduleCtrl.js
+++ www/js/controllers/ScheduleCtrl.js
@@ -1,17 +1,49 @@
-﻿app.controller('ScheduleCtrl', function ($scope, $stateParams, ionicMaterialInk, sessionService, $timeout, $ionicPopup) {
+﻿app.controller('ScheduleCtrl', function ($scope, $stateParams, ionicMaterialInk, sessionService, $timeout, $ionicPopup, $window, $q, $ionicLoading) {
   //ionic.material.ink.displayEffect();
   ionicMaterialInk.displayEffect();
 
   $scope.events = [];
   $scope.selectTab = function (index) {
     $timeout(function () {
-      console.log("index", index);
-      if (!$scope.events[index - 1]) {
-        sessionService.getList(index).then(function (response) {
-          $scope.events[index - 1] = response;
+      var dataForDay = $window.localStorage['devcon2016-day-' + index];
+      if (!dataForDay) {
+        $ionicLoading.show();
+        sessionService.getList(index + 1).then(function (response) {
+          $scope.saveResponse(index, response);
+          $ionicLoading.hide();
         });
+      } else {
+        $scope.events[index] = JSON.parse(dataForDay);
       }
     });
+  };
+
+  $scope.refreshData = function () {
+    // separate calls, unfortunately
+    var promises = [];
+    promises[0] = sessionService.getList(1);
+    promises[0].then(function (response) {
+      $scope.saveResponse(0, response);
+    });
+
+    promises[1] = sessionService.getList(2);
+    promises[1].then(function (response) {
+      $scope.saveResponse(1, response);
+    });
+
+    promises[2] = sessionService.getList(3);
+    promises[2].then(function (response) {
+      $scope.saveResponse(2, response);
+    });
+
+    $q.all(promises).then(function () {
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  $scope.saveResponse = function (index, response) {
+    $window.localStorage['devcon2016-day-' + index] = JSON.stringify(response);
+    $scope.events[index] = response;
   };
 
   $scope.showEventDetails = function (event) {
@@ -27,4 +59,16 @@
       template: event.abstract
     });
   };
+
+  $scope.speakerImages = [
+    {'chervine': 'img/speakers/ChervineBhiwoo.jpg'},
+    {'clement': 'img/speakers/ClementKerneur.png'},
+    {'dhiruj': 'img/speakers/DhirujRambaran.jpg'},
+    {'eric': 'img/speakers/EricCoquelin.jpg'},
+    {'ish': 'img/speakers/IshSookun.jpg'}
+  ];
+
+  $scope.speakerImage = function (name) {
+
+  }
 });
