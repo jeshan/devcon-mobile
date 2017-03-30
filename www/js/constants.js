12bdb4944fc48b507dbe300366cf58e1e8a5d901
jeshan <j@code.jeshan.co>
2017-03-30 23:24:15 +0400

    add messages

---
+++ www/js/constants.js
@@ -0,0 +1,22 @@
+app.constant('extraSessions', {
+  'linux': {
+    'title': 'Linux InstallFest',
+    'template': 'The Linux User Group of Mauritius is one of the few Linux User Groups in the Republic of Mauritius, quite possibly the only active one. We are registered with the Registrar of Associations under the name "Linux User Group Meta" since 2009. We all share a common interest, Linux, GNU and Free Software. The LUGM is open to all those in Mauritius who are curious about Linux, users of Linux (new or seasoned) and lovers of free software/open source in general. <br/><br/>RDV: Saturday 09:00-17:00 at Flying Dodo'
+  },
+  'makercon': {
+    'title': 'MakerCon',
+    'template': 'The Mauritius Makers community is a group of enthusiasts and passionate people, who get their hands dirty by engineering things, enhancing existing things, finding solutions to problems etc.. Although makers encompasses a diverse range of disciplines or community is geared primarily toward technological innovation. If you are interested in SBC, SoC, Arduino, ESP8266, Raspberry Pi, GPIO, Intel Edison, sensors, actuators, IoT it would be a pleasure to have you in our community. Come join us at Developers Conference 2017.<br/><br/>RDV: Friday 09:00-17:00 at Flying Dodo'
+  },
+  'drupal': {
+    'title': 'Drupal 8 Workshop',
+    'template': 'Drupal must be one of the most used PHP CMS in Mauritius and with the advent of Drupal 8, the community just keeps growing. The lack of communication and local support for Drupal in Mauritius gave rise to DrupalMauritius. This newly minted user group will be about knowledge sharing, workshops and meetups around the Drupal 8 framework. The kickoff of DrupalMauritius will be during the Developers Conference where we will be doing a hands-on Drupal 8 initiation at the Flying Dodo venue on Thursday 30th March. Please join our community on http://drupalmauritius.com<br/><br/>RDV: Thursday 14:30-17:30 in Educator 1'
+  },
+  'atlassian': {
+    'title': 'Atlassian User Group',
+    'template': 'We\'ll be launching the Mauritius AUG on 31/03/17 in a social get together format at the Flying Dodo Bagatelle Mall. If your organisation or you would like to join the first of its kind \'Agile Platform\' in Mauritius where we\'ll share our experience using Agile Methodologies and learn to improve our agile skills, I invite you to register on https://aug.atlassian.com/mauritius as an individual participant, local partner or speaker.<br/><br/>RDV: Thursday 18:00 - 21:00 at Flying Dodo'
+  },
+  'networking': {
+    'title': 'Networking & Community',
+    'template': 'Communication with your environment is an essential part of everyone\'s life. And it doesn\'t matter whether you are actually living in a rural area in the middle of nowhere, within the pulsating heart of a big city, or in my case on a wonderful island in the Indian Ocean. The ability to exchange your thoughts, your experience and your worries with another person helps you to get different points of view and new ideas on how to resolve an issue you might be confronted with. Take the chance to exchange with other attendees in the reception area or on the balcony. Have some interesting conversations about software development, latest information gained during one of the presentations and let others know about your ups & downs in your projects. Also, join the Mauritius Software Craftsmanship Community and be part of the local IT community.'
+  }
+});
